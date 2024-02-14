3114c3af9fafc9445bccd1d4a929053ee72e0c05
Shinichi Haramizu <shinichi.haramizu@sitecore.com>
2024-02-14 16:33:16 +0900

    change index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,26 @@
 import { FunctionComponent } from "react";
 import { useOcSelector } from "@/ordercloud/redux/ocStore";
+import Link from "next/link";
 import { Button } from "@nextui-org/button";
 
 const Home: FunctionComponent = () => {
   const user = useOcSelector((s) => s.ocUser.user);
 
   return (
-    <div>
-      <main>
-        <h1>React Headstart</h1>
+    <main>
+      <h1>React Headstart</h1>
 
-        <p>OrderCloud shopping experience built on React</p>
-        <div>
-          <Button>Click me</Button>
-        </div>
+      <p>OrderCloud shopping experience built on React</p>
+      <Button as={Link} href="/products/">
+        Click me
+      </Button>
 
-        {user && (
-          <pre>
-            <code>{JSON.stringify(user, null, 2)}</code>
-          </pre>
-        )}
-      </main>
-    </div>
+      {user && (
+        <pre>
+          <code>{JSON.stringify(user, null, 2)}</code>
+        </pre>
+      )}
+    </main>
   );
 };
 
